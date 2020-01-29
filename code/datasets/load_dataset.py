05ce87cb318b7bcd4fae7b8d57fe1607fd714f76
Rafael Gomes Braga <faerugb@gmail.com>
2020-01-28 19:59:22 -0500

    Added sep parameter to the load functions

--- code/datasets/load_dataset.py
+++ code/datasets/load_dataset.py
@@ -1,24 +1,25 @@
 import pandas as pd
 
 
-def load_dataset(path, header=True, x_col_indices=slice(-1), y_col_indices=-1):
-    dataset = pd.read_csv(path, header=header)
+def load_dataset(path, header='infer', sep=',', x_col_indices=slice(-1), y_col_indices=-1):
+    dataset = pd.read_csv(path, header=header, sep=sep)
     X = dataset.iloc[:, x_col_indices].values
     y = dataset.iloc[:, y_col_indices].values
     return X, y
 
 
 def load_ionosphere():
-    return load_dataset('data/adult/adult.data', None)
+    return load_dataset('data/adult/adult.data', header=None)
 
 
 def load_adult():
-    return load_dataset('data/ionosphere/ionosphere.data', None)
+    return load_dataset('data/ionosphere/ionosphere.data', header=None)
 
 
 def load_wine():
-    return load_dataset('data/wine-quality/winequality-red.csv')
+    return load_dataset('data/wine-quality/winequality-red.csv', sep=';')
 
 
 def load_cancer():
-    return load_dataset('data/breast-cancer-wisconsin/breast-cancer-wisconsin.data', None)
+    return load_dataset('data/breast-cancer-wisconsin/breast-cancer-wisconsin.data', header=None)
+
