16b3eac12f2c6ae42a37edd8004cf92e231191ea
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2020-01-28 19:57:17 -0500

    Loading datasets in Main.py

--- code/main.py
+++ code/main.py
@@ -1,5 +1,32 @@
+import pandas as pd
+
+
+def load_dataset(path, header=True, x_col_indices=slice(-1), y_col_indices=-1):
+    dataset = pd.read_csv(path, header=header)
+    X = dataset.iloc[:, x_col_indices].values
+    y = dataset.iloc[:, y_col_indices].values
+    return X, y
+
+
+def load_ionosphere():
+    return load_dataset('datasets/data/adult/adult.data', None)
+
+
+def load_adult():
+    return load_dataset('datasets/data/ionosphere/ionosphere.data', None)
+
+
+def load_wine():
+    return load_dataset('datasets/data/wine-quality/winequality-red.csv')
+
+
+def load_cancer():
+    return load_dataset('datasets/data/breast-cancer-wisconsin/breast-cancer-wisconsin.data', None)
+
+
 def run_logistic_regression():
-    pass
+    X, y = load_adult()
+    print(X, y)
 
 
 def run_naive_bayes():
