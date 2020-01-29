6ac2c26d01fdd0f743a2efa926ade4987b0f7597
Rafael Gomes Braga <faerugb@gmail.com>
2020-01-28 19:40:54 -0500

    Added specific functions to load each dataset

--- code/datasets/load_dataset.py
+++ code/datasets/load_dataset.py
@@ -1,8 +1,24 @@
 import pandas as pd
 
 
-def load_dataset(path, header, x_col_indices, y_col_indices=-1):
+def load_dataset(path, header=True, x_col_indices=slice(-1), y_col_indices=-1):
     dataset = pd.read_csv(path, header=header)
-    X = dataset.iloc[:, x_col_indices].values.to_numpy()
-    y = dataset.iloc[:, y_col_indices].values.to_numpy()
+    X = dataset.iloc[:, x_col_indices].values
+    y = dataset.iloc[:, y_col_indices].values
     return X, y
+
+
+def load_ionosphere():
+    return load_dataset('data/adult/adult.data', None)
+
+
+def load_adult():
+    return load_dataset('data/ionosphere/ionosphere.data', None)
+
+
+def load_wine():
+    return load_dataset('data/wine-quality/winequality-red.csv')
+
+
+def load_cancer():
+    return load_dataset('data/breast-cancer-wisconsin/breast-cancer-wisconsin.data', None)
