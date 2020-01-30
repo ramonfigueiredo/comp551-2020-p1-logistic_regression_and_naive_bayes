2e46edf77bf890a9d3ab4833399a13a876b5871a
Ege O <ege.odaci@mail.mcgill.ca>
2020-01-30 02:43:29 -0500

    Created plotData and added the beginning for introduction.

---
+++ code/metrics/plotData.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from datasets.load_dataset import get_dataset, Datasets
+
+def plot_error_vs_averagesplit():
+
+    getfile = pd.read_csv("datasets/data/adult/adult.data")
+plt.figure()
+
+
+#for ind, column in enumerate(getfile.columns) :
+
+
+
+
+
+
+
+
