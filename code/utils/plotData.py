214c96a8cc7d38f29211f85c34291d45889d0624
Ege O <ege.odaci@mail.mcgill.ca>
2020-01-30 19:44:37 -0500

    improved graphings

--- code/utils/plotData.py
+++ code/utils/plotData.py
@@ -5,23 +5,24 @@ import numpy as np
 from scipy.interpolate import interp1d
 
 from datasets.load_dataset import get_dataset, Datasets
+df = pd.read_csv("../datasets/data/ionosphere/ionosphere.data")
 
-df = pd.read_csv("../datasets/data/breast-cancer-wisconsin/breast-cancer-wisconsin.data")
-
-X = df.iloc[:, 1].values
+X = df.iloc[:, 34].values
 
 y = df.iloc[:,2].values
 
+f = plt.figure()
+
 #For spline data
-xx = np.linspace(X.min(), X.max(), 1000)
-y_smooth = interp1d(X, y)(xx)
+#xx = np.linspace(X.min(), X.max(), 1000)
+#y_smooth = interp1d(X, y)(xx)
 #y_smooth = interp1d(x, y, kind="cubic")(xx)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(xx, y_smooth, "r-")
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#ax.plot(xx, y_smooth, "r-")
 
-
+#For Scatter Plot
 plt.scatter(X,y)
 
 plt.show()
@@ -31,6 +32,10 @@ plt.show()
 #Histogram
 #plt.hist(X, alpha= 1, density = 1, facecolor = 'g')
 
-plt.scatter(X,y)
-#Scatter plot data
 
+f.savefig("foo.pdf", bbox_inches='tight')
+
+
+#For counting the occurences for graph.
+#(X =='g' ).sum()
+#(y=='b').sum()
