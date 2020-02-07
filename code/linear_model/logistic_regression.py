cb173d0fb40d8c2c439182323f2be61c8152d61b
Rafael Gomes Braga <faerugb@gmail.com>
2020-02-06 22:00:36 -0500

    Added a limit on the number of iterations in the LogisticRegression class

--- code/linear_model/logistic_regression.py
+++ code/linear_model/logistic_regression.py
@@ -3,9 +3,10 @@ import numpy as np
 
 # TODO: Add regularization
 class LogisticRegression:
-    def __init__(self, lr=.01, eps=1e-2):
+    def __init__(self, lr=.01, eps=1e-2, max_iter=5000):
         self.lr = lr
         self.eps = eps
+        self.max_iter = max_iter
         self.w = np.zeros(0)
 
     # Logistic function (sigmoid)
@@ -23,9 +24,15 @@ class LogisticRegression:
         N, D = X.shape
         self.w = np.zeros(D)
         g = np.inf
-        while np.linalg.norm(g) > self.eps:
+        i = 0
+        while np.linalg.norm(g) > self.eps and i < self.max_iter:
             g = self.gradient(X, y)
             self.w = self.w - self.lr * g
+            i += 1
+
+        print("\n\nGradient descent finished.")
+        print("Gradient norm: ", np.linalg.norm(g))
+        print("Number of iterations: ", i)
 
     # Returns the model prediction over a set of data points
     def predict(self, x_test):
