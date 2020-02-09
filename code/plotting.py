9555e6c15f9f79fea5e27b659aaf719140c400a6
Ege O <ege.odaci@mail.mcgill.ca>
2020-02-09 11:35:47 -0500

    Completed scatter graph seperated with output is 0 or 1

--- code/plotting.py
+++ code/plotting.py
@@ -80,17 +80,44 @@ def scatterPlot(X, Xindex, yx, yxindex):
 def scatterPlotwithY(X, Xindex, yx, yxindex, y):
     f = plt.figure()
     ax1 = f.add_subplot(111)
-    tempzero = np.zeros(shape=(len(y), 2))
-    tempone = np.zeros(shape=(len(y), 2))
-    for i in range(len(y)):
-        if y[i] == 0:
-            tempzero[i,0] = X[i, Xindex]
-            tempzero[i,1] = yx[i, yxindex]
-        else :
-            tempone[i,0] = X[i, Xindex]
-            tempone[i,1] = yx[i, yxindex]
+    # tempzero = np.zeros(shape=(len(y), 2))
+    # tempone = np.zeros(shape=(len(y), 2))
+    # for i in range(len(y)):
+    #     if y[i] == 0:
+    #         tempzero[i,0] = X[i, Xindex]
+    #         tempzero[i,1] = yx[i, yxindex]
+    #     else :
+    #         tempone[i,0] = X[i, Xindex]
+    #         tempone[i,1] = yx[i, yxindex]
+    # return tempone,tempzero
 
-    return tempone,tempzero
+    listpositivex1 = []
+    listpositivex2 = []
+
+    listnegative1 = []
+    listnegative2 = []
+
+
+    for i in range(len(y)):
+         if y[i] == 1:
+             listpositivex1.append(X[i, Xindex])
+             listpositivex2.append(yx[i,yxindex])
+         else:
+             listnegative1.append(X[i, Xindex])
+             listnegative2.append(yx[i,yxindex])
+    # return listpositive, listnegative
+    f = plt.figure()
+    ax1 = f.add_subplot(111)
+    ax1.scatter(listpositivex1, listpositivex2, c='r', marker='s', label='positive')
+    ax1.scatter(listnegative1, listnegative2, c='b', marker='s', label='negative')
+
+    plt.legend(loc='upper left');
+    plt.show()
+    f.savefig("fo6.pdf", bbox_inches='tight')
+
+
+
+
 
 
 
@@ -109,4 +136,4 @@ X, y = get_dataset(Datasets.IONOSPHERE)
 # histogramfeature(X.astype(float), 1)
 # histogramfory(y.astype(int))
 
-a,b = scatterPlotwithY(X, 4, X, 5, y)
+scatterPlotwithY(X, 4, X, 5, y)
