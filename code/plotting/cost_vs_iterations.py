b1cf039d1912e159919b59fa49c20eec4fc82ec9
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2020-02-11 00:05:48 -0500

    Including new cost_vs_iterations plotting.

--- code/plotting/cost_vs_iterations.py
+++ code/plotting/cost_vs_iterations.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
         X_train, X_test = feature_scaling(X_train, X_test)
 
         for lr in (.1, .5, 1):
+        # for lr in (0.0001, 0.001, 0.01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1):
             classifier.lr = lr
             # Fit the model to the dataset
             classifier.fit(X_train, y_train)
@@ -34,6 +35,9 @@ if __name__ == '__main__':
             plt.plot(range(len(classifier.cost_history)), classifier.cost_history)
 
         plt.legend(['lr = .1', 'lr = .5', 'lr = 1'], loc='upper right')
+        # plt.legend(
+        #     ['lr = .0001', 'lr = .001', 'lr = .01', 'lr = .1', 'lr = .2', 'lr = .3', 'lr = .4', 'lr = .5', 'lr = .6',
+        #      'lr = .7', 'lr = .8', 'lr = .9', 'lr = 1'], loc='upper right')
         plt.title(dataset_name)
         plt.xlim((0, 100))
         plt.ylabel('Cost')
